908ec416d4cf08346aad888852dc3783c187db19
Sama67676 <hairoo.kit@gmail.com>
2023-04-13 13:16:01 +0300

    api task

--- library/api.py
+++ library/api.py
@@ -1,19 +1,34 @@
 from ninja import NinjaAPI
 from ninja import Router
-
+from library.models import Product
+from library.models import BookAuth
 
 Pr_Router = Router(tags=["Produc"])
 Ath_Router = Router(tags=["book_Auth"])
+book_Router = Router(tags=["Produc"])
+
+# 1- Endpoint To Get All Available Books.
+@Pr_Router.get("allbook")
+def allbook(request):
+    return Product.objects.all().filter(is_DrawTool=False)
 
 
-@Pr_Router.get("hi")
-def hi(request):
-    num1 = 2
-    num2 = 3
-    sum = num1 + num2
-    return {"Sum" : sum}
+#2- Endpoint To Get Book With ID.
+@Pr_Router.get("bookid{id}")
+def bookid(request, id: int):
+    try:
+        product = Product.objects.get(id=id)
+        return {"ok"}
+    except Product.DoesNotExist as e:
+        return {"message": "product does not exist"}
 
 
-@Ath_Router.get("hello")
-def hello(request):
-    return None
+#3- Endpoint To Get Auth With Specific Name.
+@Ath_Router.get("authbyname{name}")
+def authbyname(request, authname=None):
+     try:
+         auth= BookAuth.objects.get(name=authname)
+         return {"ok"}
+     except BookAuth.DoesNotExist as e:
+         return {"message": "This Auth does not exist"}
+
